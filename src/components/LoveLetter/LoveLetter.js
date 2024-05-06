4a45cd5cd16eba971d11d88966c7b588a1124889
JOHNAPPARI <Johnappari123@gmail.com>
2024-05-06 11:56:04 +0530

    init

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -40,7 +40,6 @@ const LoveLetter = () => {
         Never have i been so blessed as to fall in love with someone as wonderful as you...<br />i loveeee youuuu :)<br />
         always yours,<br />
         @Me🖤You */}
-        SORRY 
       </div>
       <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
     </div>
